9d50a6f2fc55324e048db93c45511512a6f2e371
Mike Hurd <leahcim77@gmail.com>
2015-04-04 12:50:36 +0100

    Initial demonstration implementation

--- src/simple_neuron.erl
+++ src/simple_neuron.erl
@@ -10,4 +10,43 @@
 -author("mhurd").
 
 %% API
--export([]).
+-export([create/0, sense/1, loop/1]).
+
+create() ->
+  Weights = [random:uniform()-0.5, random:uniform() - 0.5, random:uniform() - 0.5],
+  register(neuron, spawn(?MODULE, loop, [Weights])).
+
+loop(Weights) ->
+  receive
+    {From, Input} ->
+      io:format("****Processing****~n Input:~p~n Using Weights:~p~n", [Input, Weights]),
+      Dot_Product = dot(Input, Weights, 0),
+      Output = [math:tanh(Dot_Product)],
+      From ! {result, Output},
+      loop(Weights);
+      Msg -> io:format("****Unknown message****~n Msg:~w~n", [Msg])
+  end.
+
+% The dot product function works on the assumption that the Bias (if present) is incorporated in the Weights vector as
+% the last element. After calculating the dot product the Input vector will be empty and the Bias will be left in the
+% Weights (if there is one).
+
+dot([I|Input], [W|Weights], Acc) ->
+  dot(Input, Weights, I*W+Acc);
+dot([], [Bias], Acc) ->
+  Acc+Bias;
+dot([],[], Acc) ->
+  Acc.
+
+sense(Signal) ->
+  case is_list(Signal) and (length(Signal) == 2) of
+    true ->
+      neuron ! {self(), Signal},
+      receive
+        {result, Output} ->
+          io:format(" Output: ~p~n", [Output])
+      end;
+    false ->
+      io:format("The Signal must be a list of length 2")
+  end.
+
